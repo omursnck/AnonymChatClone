86f65ef00d925976121c5d438156171f120cdea4
omursnck <omrsnr12@icloud.com>
2023-01-18 20:15:36 +0300

    C2 Clone Practice

---
+++ C2Clone/C2Clone/C2CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  C2CloneApp.swift
+//  C2Clone
+//
+//  Created by Ömür Şenocak on 18.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct C2CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
